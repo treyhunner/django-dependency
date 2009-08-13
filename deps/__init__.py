5d5f861f68e563f7350742f527a8dfc5b4781721
copelco@montgomery.local <none@none>
2009-08-12 22:16:15 -0400

    guess app_name be default

--- deps/__init__.py
+++ deps/__init__.py
@@ -15,39 +15,45 @@ class MissingDependency(Exception):
 
 
 class VersionControl(object):
-    def __init__(self, app_name, url, root, project_name=None):
-        self.app_name = app_name
+    def __init__(self, url, root, app_name='', project_name=''):
         self.url = url
         self.root = root
-        if not project_name:
-            path = urlparse.urlparse(self.url)[2]
-            if not path.endswith('/'):
-                path = path + '/'
-            head, tail = os.path.split(path)
-            self.project_name = os.path.basename(head)
+        tail = os.path.basename((urlparse.urlparse(url)[2]).rstrip('/'))
+        if not app_name:
+            self.app_name = tail
+            self.project_name = tail
         else:
-            self.project_name = project_name
-        self.path = os.path.join(self.root, self.project_name)
+            self.app_name = app_name
+            self.project_name = tail
+        self.python_path = os.path.join(
+            self.root,
+            self.project_name,
+        )
+        self.path = os.path.join(
+            self.root,
+            self.project_name,
+            self.app_name,
+        )
     
     def __repr__(self):
-        return self.app_name
+        return "<VersionControl: %s>" % self.app_name
     
     def add_to_python_path(self):
-        if not os.path.exists(self.path):
+        if not os.path.exists(self.python_path):
             raise MissingDependency('%s does not exist.  Run "./manage.py up" to retrieve this dependency' % self.app_name)
-        sys.path.insert(0, self.path)
+        sys.path.insert(0, self.python_path)
 
 
 class HG(VersionControl):
     def checkout(self):
         logger.info('checking out %s' % self.project_name)
-        os.system('hg clone %s %s' % (self.url, self.path))
+        os.system('hg clone %s %s' % (self.url, self.python_path))
     
     def up(self):
-        if not os.path.exists(self.path):
+        logger.info('%s' % self)
+        if not os.path.exists(self.python_path):
             self.checkout()
-        logger.info('updating %s' % self.project_name)
-        os.chdir(self.path)
+        os.chdir(self.python_path)
         os.system('hg update')
 
 
@@ -57,7 +63,7 @@ class SVN(VersionControl):
         os.system('svn co %s %s' % (self.url, self.path))
     
     def up(self):
+        logger.info('%s' % self)
         if not os.path.exists(self.path):
             self.checkout()
-        logger.info('updating %s' % self.project_name)
         os.system('svn up %s' % self.path)
