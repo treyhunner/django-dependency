a8a2f7b4bd1786a59c6f8d133967d10fcbc2b8c9
tobias@red <none@none>
2009-08-24 11:17:34 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-dependency',
+      version='1.0',
+      description='Django app to help manage external dependencies',
+      author='Caktus Consulting Group, LLC',
+      author_email='solutions@caktusgroup.com',
+      url='http://code.google.com/p/django-dependency/',
+      packages=['deps'],
+)
+
